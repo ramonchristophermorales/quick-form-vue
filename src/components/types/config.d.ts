9e344fd2d36a01673c9b0ab796e586eed6643887
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2024-05-27 03:54:37 +0800

    added reminder

--- src/components/types/config.d.ts
+++ src/components/types/config.d.ts
@@ -1,6 +1,7 @@
 import type { FormAttributes } from '@/components/types/form'
 import type { InputAttributes } from '@/components/types/input'
 
+// @todo: need to fix Config type
 export type Config = FormAttributes & {
   items?: ConfigItem[]
 }
