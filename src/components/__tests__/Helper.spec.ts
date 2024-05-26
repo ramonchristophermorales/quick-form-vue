dbc376b2b9ce28f18e22bc3245087065ed757afe
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2024-05-27 02:58:10 +0800

    added isKeyOfType unit test and some update to test

--- src/components/__tests__/Helper.spec.ts
+++ src/components/__tests__/Helper.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test, vi, beforeAll, afterAll } from 'vitest'
 
-import { errorLog, typeCheck } from '@/helper'
+import { errorLog, typeCheck, isKeyOfType } from '@/helper'
 
-describe('Helper {errorLog}', () => {
+describe('Helper errorLog function', () => {
   const testErrorMessage: string = 'test error message'
 
   let consoleErrorSpy: any
@@ -48,7 +48,7 @@ describe('Helper {errorLog}', () => {
   })
 })
 
-describe('Helper {typeCheck}', () => {
+describe('Helper typeCheck function', () => {
   type TestType = {
     string: string
     number: number
@@ -99,5 +99,77 @@ describe('Helper {typeCheck}', () => {
     expect(typeCheck<TestType>(testObjSuccess, 123 as any)).toBe(false)
     expect(typeCheck<TestType>(testObjSuccess, true as any)).toBe(false)
     expect(typeCheck<TestType>(testObjSuccess, testObjSFail as any)).toBe(false)
+  })
+
+  test('should return true if argument obj is of type T and argument knownProps is valid', () => {
+    expect(typeCheck<TestType>(testObjSuccess, testKnownProps)).toBe(true)
+  })
+})
+
+describe('Helper isKeyOfType function', () => {
+  type TestType = {
+    string: string
+    number: number
+    object: object
+  }
+
+  const validKey: keyof TestType = 'string'
+  const invalidKey: string = 'invalid-key'
+
+  const validProps: (keyof TestType)[] = ['string', 'number']
+  const inValidProps = ['invalid-key', 'invalid-key-2']
+
+  test('should be a function', () => {
+    expect(isKeyOfType).toBeTypeOf('function')
+  })
+
+  test('should return false if argument key is not string', () => {
+    expect(isKeyOfType(0 as any, validProps)).toBe(false)
+    expect(isKeyOfType(true as any, validProps)).toBe(false)
+    expect(isKeyOfType([] as any, validProps)).toBe(false)
+    expect(isKeyOfType({} as any, validProps)).toBe(false)
+  })
+
+  test('should accept argument key as string', () => {
+    const isKeyOfTypeSpy = vi.fn(isKeyOfType)
+
+    isKeyOfTypeSpy(validKey as never, validProps as never)
+
+    expect(isKeyOfTypeSpy).toHaveBeenCalledWith(validKey, validProps)
+  })
+
+  test('should accept argument key as string', () => {
+    const isKeyOfTypeSpy = vi.fn(isKeyOfType)
+
+    isKeyOfTypeSpy(validKey as never, [] as never)
+
+    expect(isKeyOfTypeSpy).toHaveBeenCalledWith(validKey, [])
+  })
+
+  test('should accept argument props as array', () => {
+    const isKeyOfTypeSpy = vi.fn(isKeyOfType)
+
+    isKeyOfTypeSpy(validKey as never, validProps as never)
+
+    expect(isKeyOfTypeSpy).toHaveBeenCalledWith(validKey, validProps)
+  })
+
+  test('should return false if argument props is not an array', () => {
+    expect(isKeyOfType<TestType>(validKey, '' as any)).toBe(false)
+    expect(isKeyOfType<TestType>(validKey, 123 as any)).toBe(false)
+    expect(isKeyOfType<TestType>(validKey, {} as any)).toBe(false)
+    expect(isKeyOfType<TestType>(validKey, null as any)).toBe(false)
+  })
+
+  test('should return false if key is invalid', () => {
+    expect(isKeyOfType<TestType>(invalidKey as any, validProps)).toBe(false)
+  })
+
+  test('should return false if props is invalid', () => {
+    expect(isKeyOfType<TestType>(validKey, inValidProps as any)).toBe(false)
+  })
+
+  test('should return true if key is valid and props is valid and key is in props', () => {
+    expect(isKeyOfType<TestType>(validKey, validProps)).toBe(true)
   })
 })
