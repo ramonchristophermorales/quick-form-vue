39f594842fd3ef63920bba49de663dc7ff72519b
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2024-06-23 17:01:08 +0800

    rename TConfig type

--- src/components/__tests__/types/Config.spec.ts
+++ src/components/__tests__/types/Config.spec.ts
@@ -1,16 +1,17 @@
 import { describe, expect, test } from 'vitest'
 
-import type { Config, ConfigItem } from '@/components/types/config'
+import type { TConfig, TConfigItem } from '@/components/types/config'
 
-describe('Config', () => {
+describe('TConfig', () => {
   test('should have the right properties and data type', () => {
-    const items: ConfigItem[] = [
+    const items: TConfigItem[] = [
       {
-        name: 'name'
+        name: 'name',
+        tagName: 'input'
       }
     ]
 
-    const config: Config = {
+    const config: TConfig = {
       action: 'action',
       acceptCharset: 'accept-charset',
       autocomplete: 'autocomplete',
@@ -29,11 +30,11 @@ describe('Config', () => {
     expect(config.enctype).toBeTypeOf('string')
     expect(config.method).toBeTypeOf('string')
     expect(config.target).toBeTypeOf('string')
-    expect(config.items?.[0]).toMatchObject<ConfigItem>(items[0])
+    expect(config.items?.[0]).toMatchObject<TConfigItem>(items[0])
   })
 
   test('should enforce required properties', () => {
-    const config: Config = {
+    const config: TConfig = {
       action: 'action'
     }
 
@@ -41,7 +42,7 @@ describe('Config', () => {
   })
 
   test('should allow missing optional properties', () => {
-    const config: Config = {
+    const config: TConfig = {
       action: 'action',
       acceptCharset: undefined,
       autocomplete: undefined,
