27fe7210abea52aa27a0504f39304ef5cef639c8
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2024-06-23 17:00:39 +0800

    init input component

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import type { TFormAttributes } from '@/components/types/form';
+
+const props = defineProps({})
+
+const attributes: Partial<TFormAttributes> = {} as Partial<TFormAttributes>;
+
+// check if the property type is defined and valid
+// check if name property is defined
+// check the other properties if they are valid as attributes
+
+</script>
+
+<template>
+    <input v-bind="attributes" />
+</template>
