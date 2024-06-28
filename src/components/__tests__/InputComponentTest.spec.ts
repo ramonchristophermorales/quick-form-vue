cf191b23b68bc7ff22b5865b7f2023f989231857
Ramon Christopher Morales <ramonchristophermorales@gmail.com>
2024-06-28 23:17:58 +0800

    add InputComponent init to diplay html output

--- src/components/__tests__/InputComponentTest.spec.ts
+++ src/components/__tests__/InputComponentTest.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, assert, test, vi, beforeAll, afterAll } from 'vitest'
+import { describe, expect, test, vi, assert } from 'vitest'
 import { shallowMount, VueWrapper } from '@vue/test-utils'
 
 import InputComponent from '@/components/InputComponent.vue'
@@ -19,6 +19,60 @@ const getWrapper = <T extends object | undefined>(additionalProps?: T): VueWrapp
 
   return wrapper
 }
+
+describe('InputComponent html', () => {
+  test('should renders input tag', () => {
+    const wrapper = getWrapper(componentAttributesTestData)
+
+    const input = wrapper.find('input')
+
+    expect(input)
+  })
+
+  test('no input attributes redered if no other props or no conditional props are passed', () => {
+    const wrapper = getWrapper(componentAttributesTestData)
+
+    const input = wrapper.find('input')
+
+    assert.isNotOk(input.attributes('accept-charset'))
+    assert.isNotOk(input.attributes('autocomplete'))
+    assert.isNotOk(input.attributes('class'))
+    assert.isNotOk(input.attributes('enctype'))
+    assert.isNotOk(input.attributes('method'))
+    assert.isNotOk(input.attributes('target'))
+    assert.isNotOk(input.attributes('value'))
+  })
+
+  test('should renders input attributes', () => {
+    const additionalProps = {
+      'test-attribute': 'test-input-name',
+      'data-id': 'test-input-name',
+      'data-value': 'test-input-name',
+      value: 'test-input-name'
+    }
+
+    const wrapper = getWrapper({
+      ...componentAttributesTestData,
+      ...additionalProps
+    })
+
+    const input = wrapper.find('input')
+
+    assert.isOk(input.attributes('type'))
+    assert.isOk(input.attributes('name'))
+    assert.isOk(input.attributes('test-attribute'))
+    assert.isOk(input.attributes('data-id'))
+    assert.isOk(input.attributes('data-value'))
+    assert.isOk(input.attributes('value'))
+
+    expect(input.attributes('type')).toBe(componentAttributesTestData.type)
+    expect(input.attributes('name')).toBe(componentAttributesTestData.name)
+    expect(input.attributes('test-attribute')).toBe('test-input-name')
+    expect(input.attributes('data-id')).toBe('test-input-name')
+    expect(input.attributes('data-value')).toBe('test-input-name')
+    expect(input.attributes('value')).toBe('test-input-name')
+  })
+})
 
 describe('InputComponent validateName', () => {
   test('should return void', () => {
